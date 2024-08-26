eba031be90ecc193959a6ddb57b359d459bd5fea
Rishu <shrivastava.rishu5@gmail.com>
2024-08-26 12:24:25 +0530

    Final Version of Builder DP

--- src/main/java/org/designpattern/Builder/Client.java
+++ src/main/java/org/designpattern/Builder/Client.java
@@ -3,13 +3,22 @@ package org.designpattern.Builder;
 public class Client {
     public static void main(String[] args) {
 
-    Builder bldr= Student.getBuilder();
-    bldr.age=10;
-    bldr.psp=55;
-    bldr.name="Builder";
-    bldr.email="bldr@designpattern.com";
-    bldr.pno="999";
+//    Student.Builder bldr= Student.getBuilder();
+//    bldr.age=10;
+//    bldr.psp=55;
+//    bldr.name="Builder";
+//    bldr.email="bldr@designpattern.com";
+//    bldr.pno="999";
+//
+//    Student st1= bldr.build();
 
-    Student st1= bldr.build();
-    Student st= new Student(bldr);
-}}
+        Student builder = Student.getBuilder()
+                .setAge(10)
+                .setName("builder")
+                .setEmail("builder@g.com")
+                .setPno("999999")
+                .setPsp(100)
+                .build();
+
+        System.out.println(builder);
+    }}
--- src/main/java/org/designpattern/Builder/Student.java
+++ src/main/java/org/designpattern/Builder/Student.java
@@ -27,7 +27,7 @@ public class Student {
         return pno;
     }
 
-    public Student(Builder bldr){
+    private Student(Builder bldr){
         this.age=bldr.age;
         this.psp=bldr.psp;
         this.name=bldr.name;
@@ -35,7 +35,63 @@ public class Student {
         this.pno=bldr.pno;
     }
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", psp=" + psp +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", pno='" + pno + '\'' +
+                '}';
+    }
+
     public static Builder getBuilder(){
         return new Builder();
+    }
+
+    public static class Builder {
+        private int age;
+        private int psp;
+        private String name;
+        private String email;
+        private String pno;
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder setPsp(int psp) {
+            this.psp = psp;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setPno(String pno) {
+            this.pno = pno;
+            return this;
+        }
+
+        public Student build(){
+            //Validation are here now
+            if(this.age<0 || this.age >120){
+                throw new IllegalArgumentException("Age should be between 0 and 120");
+            }
+
+            if(this.psp<0 || this.psp >100){
+                throw new IllegalArgumentException("PSP should be between 0 and 100");
+            }
+            return new Student(this);
+        }
     }
 }
